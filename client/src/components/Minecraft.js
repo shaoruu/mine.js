e583ad43e5bc25b59e7cc179d4b6a5d91772ce60
dvfcfbgv <21ianh1@tas.tw>
2019-03-18 09:46:52 +0800

    added more file structures

--- client/src/components/Minecraft.js
+++ client/src/components/Minecraft.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
-class App extends Component {
+class Minecraft extends Component {
 	render() {
 		return <div className="App">lol</div>
 	}
 }
 
-export default App
+export default Minecraft
