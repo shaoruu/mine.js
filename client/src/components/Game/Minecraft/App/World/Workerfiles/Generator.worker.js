e97888728b769dec353ab3ccb193d455e66426c6
dvfcfbgv <21ianh1@tas.tw>
2019-06-21 10:19:46 +0800

    minor fix for array checking

--- client/src/components/Game/Minecraft/App/World/Workerfiles/Generator.worker.js
+++ client/src/components/Game/Minecraft/App/World/Workerfiles/Generator.worker.js
@@ -101,7 +101,15 @@ export default () => {
       }
     }
 
-    this.setLightingData = (setLighting, setSmoothLighting, get, coordx, coordy, coordz, changedBlocks) => {
+    this.setLightingData = (
+      setLighting,
+      setSmoothLighting,
+      get,
+      coordx,
+      coordy,
+      coordz,
+      changedBlocks
+    ) => {
       const offsets = [coordx * size - 1, coordy * size - 1, coordz * size - 1]
 
       for (let x = 1; x < size + 1; x++)
@@ -114,17 +122,37 @@ export default () => {
                 tempy = tempCoords[1],
                 tempz = tempCoords[2]
 
-              const lighting = generator.getBlockLighting(tempx, tempy, tempz, get, offsets, changedBlocks);
+              const lighting = generator.getBlockLighting(
+                tempx,
+                tempy,
+                tempz,
+                get,
+                offsets,
+                changedBlocks
+              )
               for (let l = 0; l < 6; l++) {
-                setLighting(x - 1, z - 1, y - 1, l, lighting[l]);
+                setLighting(x - 1, z - 1, y - 1, l, lighting[l])
               }
 
-              const smoothLighting = generator.getBlockSmoothLighting(x, y, z, get)
+              const smoothLighting = generator.getBlockSmoothLighting(
+                x,
+                y,
+                z,
+                get
+              )
               for (let l = 0; l < 6; l++) {
                 if (smoothLighting[l]) {
                   for (let m = 0; m < 3; m++)
                     for (let n = 0; n < 3; n++) {
-                      setSmoothLighting(x - 1, z - 1, y - 1, l, m, n, smoothLighting[l][m][n])
+                      setSmoothLighting(
+                        x - 1,
+                        z - 1,
+                        y - 1,
+                        l,
+                        m,
+                        n,
+                        smoothLighting[l][m][n]
+                      )
                     }
                 }
               }
@@ -140,9 +168,9 @@ export default () => {
         { x: -1, y: 0, z: 0 },
         { x: 0, y: 0, z: -1 },
         { x: 0, y: -1, z: 0 }
-      ];
+      ]
 
-      const lights = new Uint16Array(surroundings.length);
+      const lights = new Uint16Array(surroundings.length)
 
       for (let i = 0; i < surroundings.length; i++) {
         const block = {
@@ -150,16 +178,23 @@ export default () => {
           y: y + surroundings[i].y,
           z: z + surroundings[i].z,
           lightLevel: 15
-        };
-        const value = this.getLoadedBlocks(block.x, block.y, block.z, get, offsets, changedBlocks)
+        }
+        const value = this.getLoadedBlocks(
+          block.x,
+          block.y,
+          block.z,
+          get,
+          offsets,
+          changedBlocks
+        )
         if (value === 0) {
-          const pastNodeCoords = [getCoordsRepresentation(block.x, -1, block.z)];
-          const queue = [block];
+          const pastNodeCoords = [getCoordsRepresentation(block.x, -1, block.z)]
+          const queue = [block]
           while (queue.length > 0) {
-            const q = queue.splice(0, 1)[0];
+            const q = queue.splice(0, 1)[0]
             if (this.getHighestBlock(q.x, q.z) < q.y) {
-              lights[i] = q.lightLevel;
-              break;
+              lights[i] = q.lightLevel
+              break
             }
             for (let n = 1; n < surroundings.length - 1; n++) {
               const newNode = {
@@ -167,46 +202,67 @@ export default () => {
                 y: -1,
                 z: q.z + surroundings[n].z,
                 lightLevel: q.lightLevel - 1
-              };
+              }
               if (
                 pastNodeCoords.indexOf(
                   getCoordsRepresentation(newNode.x, newNode.y, newNode.z)
                 ) !== -1
               ) {
-                continue;
+                continue
               }
               if (newNode.lightLevel < 0) {
-                continue;
+                continue
               }
 
-              let yValue = q.y;
+              let yValue = q.y
 
-              let startValue = 0;
+              let startValue = 0
 
-              let endValue = this.getLoadedBlocks(newNode.x, yValue, newNode.z, get, offsets, changedBlocks)
+              let endValue = this.getLoadedBlocks(
+                newNode.x,
+                yValue,
+                newNode.z,
+                get,
+                offsets,
+                changedBlocks
+              )
 
               while (startValue === 0 && endValue !== 0) {
-                yValue += 1;
-                startValue = this.getLoadedBlocks(q.x, yValue, q.z, get, offsets, changedBlocks)
-                endValue = this.getLoadedBlocks(newNode.x, yValue, newNode.z, get, offsets, changedBlocks)
+                yValue += 1
+                startValue = this.getLoadedBlocks(
+                  q.x,
+                  yValue,
+                  q.z,
+                  get,
+                  offsets,
+                  changedBlocks
+                )
+                endValue = this.getLoadedBlocks(
+                  newNode.x,
+                  yValue,
+                  newNode.z,
+                  get,
+                  offsets,
+                  changedBlocks
+                )
               }
 
               if (startValue !== 0 || endValue !== 0) {
-                continue;
+                continue
               }
 
-              newNode.y = yValue;
+              newNode.y = yValue
 
-              queue.push(newNode);
+              queue.push(newNode)
               pastNodeCoords.push(
                 getCoordsRepresentation(newNode.x, -1, newNode.z)
-              );
+              )
             }
           }
         }
       }
-      return lights;
-    };
+      return lights
+    }
 
     this.getBlockSmoothLighting = (x, y, z, get) => {
       const output = new Array(6)
@@ -244,133 +300,133 @@ export default () => {
       const pxpzpy = get(x + 1, z + 1, y + 1)
 
       if (py === 0) {
-        const a = nxpy !== 0 || nzpy !== 0 || nxnzpy !== 0 ? 0 : 1;
-        const b = nxpy !== 0 || pzpy !== 0 || nxpzpy !== 0 ? 0 : 1;
-        const c = pxpy !== 0 || pzpy !== 0 || pxpzpy !== 0 ? 0 : 1;
-        const d = pxpy !== 0 || nzpy !== 0 || pxnzpy !== 0 ? 0 : 1;
+        const a = nxpy !== 0 || nzpy !== 0 || nxnzpy !== 0 ? 0 : 1
+        const b = nxpy !== 0 || pzpy !== 0 || nxpzpy !== 0 ? 0 : 1
+        const c = pxpy !== 0 || pzpy !== 0 || pxpzpy !== 0 ? 0 : 1
+        const d = pxpy !== 0 || nzpy !== 0 || pxnzpy !== 0 ? 0 : 1
 
         if (a + c > b + d) {
           const py2ColorsFace0 = new Uint16Array(3)
-          py2ColorsFace0[0] = b === 0 ? shadow : light;
-          py2ColorsFace0[1] = c === 0 ? shadow : light;
-          py2ColorsFace0[2] = a === 0 ? shadow : light;
+          py2ColorsFace0[0] = b === 0 ? shadow : light
+          py2ColorsFace0[1] = c === 0 ? shadow : light
+          py2ColorsFace0[2] = a === 0 ? shadow : light
 
           const py2ColorsFace1 = new Uint16Array(3)
-          py2ColorsFace1[0] = c === 0 ? shadow : light;
-          py2ColorsFace1[1] = d === 0 ? shadow : light;
-          py2ColorsFace1[2] = a === 0 ? shadow : light;
+          py2ColorsFace1[0] = c === 0 ? shadow : light
+          py2ColorsFace1[1] = d === 0 ? shadow : light
+          py2ColorsFace1[2] = a === 0 ? shadow : light
 
           output[0] = [py2ColorsFace0, py2ColorsFace1, [1, 1, 1]]
         } else {
           const pyColorsFace0 = new Uint16Array(3)
-          pyColorsFace0[0] = a === 0 ? shadow : light;
-          pyColorsFace0[1] = b === 0 ? shadow : light;
-          pyColorsFace0[2] = d === 0 ? shadow : light;
+          pyColorsFace0[0] = a === 0 ? shadow : light
+          pyColorsFace0[1] = b === 0 ? shadow : light
+          pyColorsFace0[2] = d === 0 ? shadow : light
 
           const pyColorsFace1 = new Uint16Array(3)
-          pyColorsFace1[0] = b === 0 ? shadow : light;
-          pyColorsFace1[1] = c === 0 ? shadow : light;
-          pyColorsFace1[2] = d === 0 ? shadow : light;
+          pyColorsFace1[0] = b === 0 ? shadow : light
+          pyColorsFace1[1] = c === 0 ? shadow : light
+          pyColorsFace1[2] = d === 0 ? shadow : light
 
           output[0] = [pyColorsFace0, pyColorsFace1, [0, 0, 0]]
         }
       }
 
       if (px === 0) {
-        const a = pxny !== 0 || pxnz !== 0 || pxnzny !== 0 ? 0 : 1;
-        const b = pxny !== 0 || pxpz !== 0 || pxpzny !== 0 ? 0 : 1;
-        const c = pxpy !== 0 || pxpz !== 0 || pxpzpy !== 0 ? 0 : 1;
-        const d = pxpy !== 0 || pxnz !== 0 || pxnzpy !== 0 ? 0 : 1;
+        const a = pxny !== 0 || pxnz !== 0 || pxnzny !== 0 ? 0 : 1
+        const b = pxny !== 0 || pxpz !== 0 || pxpzny !== 0 ? 0 : 1
+        const c = pxpy !== 0 || pxpz !== 0 || pxpzpy !== 0 ? 0 : 1
+        const d = pxpy !== 0 || pxnz !== 0 || pxnzpy !== 0 ? 0 : 1
 
         if (a + c < b + d) {
           const px2ColorsFace0 = new Uint16Array(3)
-          px2ColorsFace0[0] = b === 0 ? shadow : light;
-          px2ColorsFace0[1] = c === 0 ? shadow : light;
-          px2ColorsFace0[2] = a === 0 ? shadow : light;
+          px2ColorsFace0[0] = b === 0 ? shadow : light
+          px2ColorsFace0[1] = c === 0 ? shadow : light
+          px2ColorsFace0[2] = a === 0 ? shadow : light
 
           const px2ColorsFace1 = new Uint16Array(3)
-          px2ColorsFace1[0] = c === 0 ? shadow : light;
-          px2ColorsFace1[1] = d === 0 ? shadow : light;
-          px2ColorsFace1[2] = a === 0 ? shadow : light;
+          px2ColorsFace1[0] = c === 0 ? shadow : light
+          px2ColorsFace1[1] = d === 0 ? shadow : light
+          px2ColorsFace1[2] = a === 0 ? shadow : light
 
           output[1] = [px2ColorsFace0, px2ColorsFace1, [1, 1, 1]]
         } else {
           const pxColorsFace0 = new Uint16Array(3)
-          pxColorsFace0[0] = c === 0 ? shadow : light;
-          pxColorsFace0[1] = b === 0 ? shadow : light;
-          pxColorsFace0[2] = d === 0 ? shadow : light;
+          pxColorsFace0[0] = c === 0 ? shadow : light
+          pxColorsFace0[1] = b === 0 ? shadow : light
+          pxColorsFace0[2] = d === 0 ? shadow : light
 
           const pxColorsFace1 = new Uint16Array(3)
-          pxColorsFace1[0] = b === 0 ? shadow : light;
-          pxColorsFace1[1] = a === 0 ? shadow : light;
-          pxColorsFace1[2] = d === 0 ? shadow : light;
+          pxColorsFace1[0] = b === 0 ? shadow : light
+          pxColorsFace1[1] = a === 0 ? shadow : light
+          pxColorsFace1[2] = d === 0 ? shadow : light
 
           output[1] = [pxColorsFace0, pxColorsFace1, [0, 0, 0]]
         }
       }
 
       if (pz === 0) {
-        const a = pzny !== 0 || nxpz !== 0 || nxpzny !== 0 ? 0 : 1;
-        const b = pzny !== 0 || pxpz !== 0 || pxpzny !== 0 ? 0 : 1;
-        const c = pzpy !== 0 || pxpz !== 0 || pxpzpy !== 0 ? 0 : 1;
-        const d = pzpy !== 0 || nxpz !== 0 || nxpzpy !== 0 ? 0 : 1;
+        const a = pzny !== 0 || nxpz !== 0 || nxpzny !== 0 ? 0 : 1
+        const b = pzny !== 0 || pxpz !== 0 || pxpzny !== 0 ? 0 : 1
+        const c = pzpy !== 0 || pxpz !== 0 || pxpzpy !== 0 ? 0 : 1
+        const d = pzpy !== 0 || nxpz !== 0 || nxpzpy !== 0 ? 0 : 1
 
         if (a + c > b + d) {
           const pz2ColorsFace0 = new Uint16Array(3)
-          pz2ColorsFace0[0] = a === 0 ? shadow : light;
-          pz2ColorsFace0[1] = b === 0 ? shadow : light;
-          pz2ColorsFace0[2] = d === 0 ? shadow : light;
+          pz2ColorsFace0[0] = a === 0 ? shadow : light
+          pz2ColorsFace0[1] = b === 0 ? shadow : light
+          pz2ColorsFace0[2] = d === 0 ? shadow : light
 
           const pz2ColorsFace1 = new Uint16Array(3)
-          pz2ColorsFace1[0] = b === 0 ? shadow : light;
-          pz2ColorsFace1[1] = c === 0 ? shadow : light;
-          pz2ColorsFace1[2] = d === 0 ? shadow : light;
+          pz2ColorsFace1[0] = b === 0 ? shadow : light
+          pz2ColorsFace1[1] = c === 0 ? shadow : light
+          pz2ColorsFace1[2] = d === 0 ? shadow : light
 
           output[2] = [pz2ColorsFace0, pz2ColorsFace1, [1, 1, 1]]
         } else {
           const pzColorsFace0 = new Uint16Array(3)
-          pzColorsFace0[0] = d === 0 ? shadow : light;
-          pzColorsFace0[1] = a === 0 ? shadow : light;
-          pzColorsFace0[2] = c === 0 ? shadow : light;
+          pzColorsFace0[0] = d === 0 ? shadow : light
+          pzColorsFace0[1] = a === 0 ? shadow : light
+          pzColorsFace0[2] = c === 0 ? shadow : light
 
           const pzColorsFace1 = new Uint16Array(3)
-          pzColorsFace1[0] = a === 0 ? shadow : light;
-          pzColorsFace1[1] = b === 0 ? shadow : light;
-          pzColorsFace1[2] = c === 0 ? shadow : light;
+          pzColorsFace1[0] = a === 0 ? shadow : light
+          pzColorsFace1[1] = b === 0 ? shadow : light
+          pzColorsFace1[2] = c === 0 ? shadow : light
 
           output[2] = [pzColorsFace0, pzColorsFace1, [0, 0, 0]]
           // output[2] = [[light, light, light], [light, shadow, light], [0, 0, 0]]
         }
       }
       if (nx === 0) {
-        const a = nxny !== 0 || nxnz !== 0 || nxnzny !== 0 ? 0 : 1;
-        const b = nxny !== 0 || nxpz !== 0 || nxpzny !== 0 ? 0 : 1;
-        const c = nxpy !== 0 || nxpz !== 0 || nxpzpy !== 0 ? 0 : 1;
-        const d = nxpy !== 0 || nxnz !== 0 || nxnzpy !== 0 ? 0 : 1;
+        const a = nxny !== 0 || nxnz !== 0 || nxnzny !== 0 ? 0 : 1
+        const b = nxny !== 0 || nxpz !== 0 || nxpzny !== 0 ? 0 : 1
+        const c = nxpy !== 0 || nxpz !== 0 || nxpzpy !== 0 ? 0 : 1
+        const d = nxpy !== 0 || nxnz !== 0 || nxnzpy !== 0 ? 0 : 1
 
         if (a + c < b + d) {
           const nx2ColorsFace0 = new Uint16Array(3)
-          nx2ColorsFace0[0] = b === 0 ? shadow : light;
-          nx2ColorsFace0[1] = a === 0 ? shadow : light;
-          nx2ColorsFace0[2] = c === 0 ? shadow : light;
+          nx2ColorsFace0[0] = b === 0 ? shadow : light
+          nx2ColorsFace0[1] = a === 0 ? shadow : light
+          nx2ColorsFace0[2] = c === 0 ? shadow : light
 
           const nx2ColorsFace1 = new Uint16Array(3)
-          nx2ColorsFace1[0] = a === 0 ? shadow : light;
-          nx2ColorsFace1[1] = d === 0 ? shadow : light;
-          nx2ColorsFace1[2] = c === 0 ? shadow : light;
+          nx2ColorsFace1[0] = a === 0 ? shadow : light
+          nx2ColorsFace1[1] = d === 0 ? shadow : light
+          nx2ColorsFace1[2] = c === 0 ? shadow : light
 
           output[3] = [nx2ColorsFace0, nx2ColorsFace1, [1, 1, 1]]
           // output[3] = [[light, light, light], [light, shadow, light], [1, 1, 1]]
         } else {
           const nxColorsFace0 = new Uint16Array(3)
-          nxColorsFace0[0] = c === 0 ? shadow : light;
-          nxColorsFace0[1] = b === 0 ? shadow : light;
-          nxColorsFace0[2] = d === 0 ? shadow : light;
+          nxColorsFace0[0] = c === 0 ? shadow : light
+          nxColorsFace0[1] = b === 0 ? shadow : light
+          nxColorsFace0[2] = d === 0 ? shadow : light
 
           const nxColorsFace1 = new Uint16Array(3)
-          nxColorsFace1[0] = b === 0 ? shadow : light;
-          nxColorsFace1[1] = a === 0 ? shadow : light;
-          nxColorsFace1[2] = d === 0 ? shadow : light;
+          nxColorsFace1[0] = b === 0 ? shadow : light
+          nxColorsFace1[1] = a === 0 ? shadow : light
+          nxColorsFace1[2] = d === 0 ? shadow : light
 
           output[3] = [nxColorsFace0, nxColorsFace1, [0, 0, 0]]
           // output[3] = [[light, light, light], [light, shadow, light], [0, 0, 0]]
@@ -378,33 +434,33 @@ export default () => {
       }
 
       if (nz === 0) {
-        const a = nzny !== 0 || nxnz !== 0 || nxnzny !== 0 ? 0 : 1;
-        const b = nzny !== 0 || pxnz !== 0 || pxnzny !== 0 ? 0 : 1;
-        const c = nzpy !== 0 || pxnz !== 0 || pxnzpy !== 0 ? 0 : 1;
-        const d = nzpy !== 0 || nxnz !== 0 || nxnzpy !== 0 ? 0 : 1;
+        const a = nzny !== 0 || nxnz !== 0 || nxnzny !== 0 ? 0 : 1
+        const b = nzny !== 0 || pxnz !== 0 || pxnzny !== 0 ? 0 : 1
+        const c = nzpy !== 0 || pxnz !== 0 || pxnzpy !== 0 ? 0 : 1
+        const d = nzpy !== 0 || nxnz !== 0 || nxnzpy !== 0 ? 0 : 1
 
         if (a + c > b + d) {
           const nz2ColorsFace0 = new Uint16Array(3)
-          nz2ColorsFace0[0] = a === 0 ? shadow : light;
-          nz2ColorsFace0[1] = b === 0 ? shadow : light;
-          nz2ColorsFace0[2] = d === 0 ? shadow : light;
+          nz2ColorsFace0[0] = a === 0 ? shadow : light
+          nz2ColorsFace0[1] = b === 0 ? shadow : light
+          nz2ColorsFace0[2] = d === 0 ? shadow : light
 
           const nz2ColorsFace1 = new Uint16Array(3)
-          nz2ColorsFace1[0] = b === 0 ? shadow : light;
-          nz2ColorsFace1[1] = c === 0 ? shadow : light;
-          nz2ColorsFace1[2] = d === 0 ? shadow : light;
+          nz2ColorsFace1[0] = b === 0 ? shadow : light
+          nz2ColorsFace1[1] = c === 0 ? shadow : light
+          nz2ColorsFace1[2] = d === 0 ? shadow : light
 
           output[4] = [nz2ColorsFace0, nz2ColorsFace1, [1, 1, 1]]
         } else {
           const nzColorsFace0 = new Uint16Array(3)
-          nzColorsFace0[0] = d === 0 ? shadow : light;
-          nzColorsFace0[1] = a === 0 ? shadow : light;
-          nzColorsFace0[2] = c === 0 ? shadow : light;
+          nzColorsFace0[0] = d === 0 ? shadow : light
+          nzColorsFace0[1] = a === 0 ? shadow : light
+          nzColorsFace0[2] = c === 0 ? shadow : light
 
           const nzColorsFace1 = new Uint16Array(3)
-          nzColorsFace1[0] = a === 0 ? shadow : light;
-          nzColorsFace1[1] = b === 0 ? shadow : light;
-          nzColorsFace1[2] = c === 0 ? shadow : light;
+          nzColorsFace1[0] = a === 0 ? shadow : light
+          nzColorsFace1[1] = b === 0 ? shadow : light
+          nzColorsFace1[2] = c === 0 ? shadow : light
 
           output[4] = [nzColorsFace0, nzColorsFace1, [0, 0, 0]]
         }
@@ -419,7 +475,13 @@ export default () => {
 
     this.getLoadedBlocks = (x, y, z, get, offsets, changedBlocks) => {
       const relativeCoords = this.getRelativeCoords(x, y, z, offsets)
-      if (this.checkWithinChunk(relativeCoords[0], relativeCoords[1], relativeCoords[2])) {
+      if (
+        this.checkWithinChunk(
+          relativeCoords[0],
+          relativeCoords[1],
+          relativeCoords[2]
+        )
+      ) {
         return get(relativeCoords[0], relativeCoords[2], relativeCoords[1])
       } else {
         return this.getBlockInfo(x, y, z, changedBlocks)
@@ -437,7 +499,7 @@ export default () => {
       else {
         const isSolid = isSolidAt(x, y, z)
         if (isSolid) {
-          return 57
+          return 2
         } else if (y <= waterLevel) {
           return 1
         }
@@ -445,10 +507,19 @@ export default () => {
       return 0
     }
 
-    this.getRelativeCoords = (x, y, z, offsets) => ([x - offsets[0], y - offsets[1], z - offsets[2]])
+    this.getRelativeCoords = (x, y, z, offsets) => [
+      x - offsets[0],
+      y - offsets[1],
+      z - offsets[2]
+    ]
 
-    this.getAbsoluteCoords = (x, y, z, offsets) => ([x + offsets[0], y + offsets[1], z + offsets[2]])
+    this.getAbsoluteCoords = (x, y, z, offsets) => [
+      x + offsets[0],
+      y + offsets[1],
+      z + offsets[2]
+    ]
 
-    this.checkWithinChunk = (x, y, z) => (x >= 0 && x < size + 2 && y >= 0 && y < size + 2 && z >= 0 && z < size + 2)
+    this.checkWithinChunk = (x, y, z) =>
+      x >= 0 && x < size + 2 && y >= 0 && y < size + 2 && z >= 0 && z < size + 2
   }
 }
--- client/src/components/Game/Minecraft/Data/ResourceManager/GeometryManagers/BlockGeometryManager.js
+++ client/src/components/Game/Minecraft/Data/ResourceManager/GeometryManagers/BlockGeometryManager.js
@@ -17,36 +17,36 @@ class BlockGeometryManager {
       this.geometries[key] = new THREE.PlaneGeometry(dimension, dimension)
       const { func, rotation } = rBlock[key]
       switch (key) {
-        case "px":
-        case "nx":
-        case "pz":
+        case 'px':
+        case 'nx':
+        case 'pz':
         case 'nz':
-        case "px2":
-        case "nx2":
-        case "pz2":
+        case 'px2':
+        case 'nx2':
+        case 'pz2':
         case 'nz2':
-          this.geometries[key].faceVertexUvs[0][0][0].y = 0.5;
-          this.geometries[key].faceVertexUvs[0][0][2].y = 0.5;
-          this.geometries[key].faceVertexUvs[0][1][2].y = 0.5;
+          this.geometries[key].faceVertexUvs[0][0][0].y = 0.5
+          this.geometries[key].faceVertexUvs[0][0][2].y = 0.5
+          this.geometries[key].faceVertexUvs[0][1][2].y = 0.5
           break
-        case "py":
-        case "ny":
-        case "py2":
-        case "ny2":
-          this.geometries[key].faceVertexUvs[0][0][1].y = 0.5;
-          this.geometries[key].faceVertexUvs[0][1][0].y = 0.5;
-          this.geometries[key].faceVertexUvs[0][1][1].y = 0.5;
+        case 'py':
+        case 'ny':
+        case 'py2':
+        case 'ny2':
+          this.geometries[key].faceVertexUvs[0][0][1].y = 0.5
+          this.geometries[key].faceVertexUvs[0][1][0].y = 0.5
+          this.geometries[key].faceVertexUvs[0][1][1].y = 0.5
           break
         default:
           break
       }
       // this.geometries[key].computeFaceNormals()
-      if (func && func.constructor === Array) {
+      if (func && Array.isArray(func)) {
         for (let i = 0; i < func.length; i++) {
           this.geometries[key][func[i]](rotation[i])
         }
       } else if (func) {
-        this.geometries[key][func](rotation);
+        this.geometries[key][func](rotation)
       }
     }
   }
@@ -54,8 +54,19 @@ class BlockGeometryManager {
   get = key => this.geometries[key]
 
   getWLighting = (key, lighting, smoothLighting) => {
-    const light = new THREE.Color(`rgb(${rLighting[lighting]},${rLighting[lighting]},${rLighting[lighting]})`)
-    const shadow = lighting > 0 ? new THREE.Color(`rgb(${rLighting[lighting - 1]},${rLighting[lighting - 1]},${rLighting[lighting - 1]})`) : light
+    const light = new THREE.Color(
+      `rgb(${rLighting[lighting]},${rLighting[lighting]},${
+        rLighting[lighting]
+      })`
+    )
+    const shadow =
+      lighting > 0
+        ? new THREE.Color(
+            `rgb(${rLighting[lighting - 1]},${rLighting[lighting - 1]},${
+              rLighting[lighting - 1]
+            })`
+          )
+        : light
     // const slDifference = 50
 
     // const light = new THREE.Color(`rgb(${lighting},${lighting},${lighting})`)
@@ -67,21 +78,21 @@ class BlockGeometryManager {
     const geo = this.geometries[key]
 
     switch (key) {
-      case "px":
-      case "nx":
-      case "pz":
+      case 'px':
+      case 'nx':
+      case 'pz':
       case 'nz':
-      case "px2":
-      case "nx2":
-      case "pz2":
+      case 'px2':
+      case 'nx2':
+      case 'pz2':
       case 'nz2':
         geo.faces[0].vertexColors = [light, shadow, light]
         geo.faces[1].vertexColors = [shadow, shadow, light]
         break
-      case "py":
-      case "ny":
-      case "py2":
-      case "ny2":
+      case 'py':
+      case 'ny':
+      case 'py2':
+      case 'ny2':
         geo.faces[0].vertexColors = [light, light, light]
         geo.faces[1].vertexColors = [light, light, light]
         break
@@ -93,7 +104,12 @@ class BlockGeometryManager {
       for (let m = 0; m < 2; m++) {
         const colors = new Array(3)
         for (let n = 0; n < 3; n++) {
-          colors[n] = smoothLighting[m][n] === 1 ? shadow : smoothLighting[m][n] === 2 ? light : shadow
+          colors[n] =
+            smoothLighting[m][n] === 1
+              ? shadow
+              : smoothLighting[m][n] === 2
+              ? light
+              : shadow
         }
         geo.faces[m].vertexColors = colors
       }
