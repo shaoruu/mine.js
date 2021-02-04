455ed4ea4f881cd97839c969bbbbfbb3a6b447cc
Ian Huang <21ianh1@students.tas.tw>
2021-02-04 23:01:00 +0800

    feat: eslint in the mix

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+export default {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    'import/order': [2, { groups: ['parent', 'sibling'], 'newlines-between': 'always' }],
+    'import/prefer-default-export': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'no-plusplus': 'off',
+    'no-return-assign': 'off',
+    'no-nested-ternary': 'off',
+    'no-param-reassign': 'off',
+    'no-continue': 'off',
+    'func-names': 'off',
+    'no-bitwise': 'off',
+    'consistent-return': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+    'no-restricted-globals': 'off',
+  },
+};
