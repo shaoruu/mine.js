d64db084ae6f836976c96ec212528ec6233738e7
ian13456 <21ianh1@students.tas.tw>
2021-07-17 21:24:30 +0800

    feat: toggle fullscreen

--- client/core/container.ts
+++ client/core/container.ts
@@ -18,7 +18,10 @@ class Container extends EventEmitter {
     super();
 
     this.setupCanvas(options);
-    this.setupListeners();
+
+    engine.on('ready', () => {
+      this.setupListeners();
+    });
   }
 
   setupCanvas = (options: Partial<ContainerOptionsType>) => {
@@ -47,9 +50,43 @@ class Container extends EventEmitter {
     window.addEventListener('blur', () => {
       this.engine.emit('blur');
     });
+
     window.addEventListener('focus', () => {
       this.engine.emit('focus');
     });
+
+    this.engine.inputs.bind('k', this.toggleFullScreen, '*');
+  };
+
+  toggleFullScreen = () => {
+    const elem = document.body;
+    if (
+      (document.fullScreenElement !== undefined && document.fullScreenElement === null) ||
+      (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) ||
+      (document.mozFullScreen !== undefined && !document.mozFullScreen) ||
+      (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)
+    ) {
+      if (elem.requestFullScreen) {
+        elem.requestFullScreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullScreen) {
+        // @ts-ignore
+        elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    } else {
+      if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
   };
 
   fitCanvas = () => {
