6da21f7c85abb3377be634445cb704173eab4522
ian13456 <21ianh1@students.tas.tw>
2021-08-05 07:24:14 +0800

    feat: biomes setup

--- client/pages/Biomes.svelte
+++ client/pages/Biomes.svelte
@@ -1,1 +1,58 @@
-<div>test</div>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+  let el: HTMLCanvasElement;
+
+  onMount(() => {
+    const scene = new Scene();
+
+    const renderer = new WebGLRenderer({ antialias: true, canvas: el });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(400, 200, 0);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      controls.update();
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    window.addEventListener('resize', onWindowResize);
+  });
+</script>
+
+<canvas bind:this={el} />
+
+<style>
+  canvas {
+    width: 100vw;
+    height: 100vh;
+    background: black;
+  }
+</style>
